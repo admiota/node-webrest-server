ab637ce69aed8859f31b281aa383fb3595cafaf3
adolfo <adolfomiota1@gmail.com>
2024-03-04 14:02:31 +0100

    ajuste deploy

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,15 +1,17 @@
+import { PrismaClient} from "@prisma/client";
 import { CreateTodoDto } from './../../domain/dtos/todos/create-todo.dto';
 //import { prisma } from '../../../prisma/data/postgres/index';
-import { prisma } from './../../../prisma/data/postgres/index';
-
-
 import { Request, Response } from "express"
 import { UpdateTodoDto } from '../../domain/dtos';
+
+//DEFINIMOS AQUÍ EL PRISMA EXPORTADO
+const prismaExportado = new PrismaClient();
+
 
 export class TodosController {
     public getTodos = async (req: Request, res: Response) => {
         
-        const todos = await prisma.todo.findMany();
+        const todos = await prismaExportado.todo.findMany();
         res.json(todos); 
     }
 
@@ -17,7 +19,7 @@ export class TodosController {
         const id = +req.params.id;
         if (isNaN(id)) return res.status(400).json({ error: `ID is not a number` });
 
-        const todo = await prisma.todo.findFirst({ where: { id: id } });
+        const todo = await prismaExportado.todo.findFirst({ where: { id: id } });
         (todo)
             ? res.json(todo)
             : res.status(404).json({ error: `Todo with id: ${id} not found.` });
@@ -28,7 +30,7 @@ export class TodosController {
         const [error, createTodoDto] = CreateTodoDto.create(req.body);
         if (error) return res.status(400).json({ error });
  
-        const todo = await prisma.todo.create({data: createTodoDto!});
+        const todo = await prismaExportado.todo.create({data: createTodoDto!});
 
         res.json(todo);
     }
@@ -38,10 +40,10 @@ export class TodosController {
         const [error, updateTodoDto] = UpdateTodoDto.create({...req.body,id});
         if (error) return res.status(400).json(error);
         
-        const todo = await prisma.todo.findFirst({where: { id: id }});
+        const todo = await prismaExportado.todo.findFirst({where: { id: id }});
         if (!todo) return res.status(404).json({ error: `Todo with id: ${id} not found` });
 
-        const updatedTodo = await prisma.todo.update(
+        const updatedTodo = await prismaExportado.todo.update(
             {
                 where: { id: id },
                 data: updateTodoDto!.values
@@ -53,10 +55,10 @@ export class TodosController {
 
     public deleteTodo = async(req: Request, res: Response) => {
         const id = +req.params.id;
-        const todo = await prisma.todo.findFirst({where: { id: id }});
+        const todo = await prismaExportado.todo.findFirst({where: { id: id }});
         if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found` });
 
-        const deletedTodo = await prisma.todo.delete({ where: { id: id } });
+        const deletedTodo = await prismaExportado.todo.delete({ where: { id: id } });
 
         (deletedTodo)
             ? res.json(deletedTodo)
